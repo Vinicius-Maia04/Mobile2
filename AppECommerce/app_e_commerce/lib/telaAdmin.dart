7e65695a5a821205cbb5b1f52b8dfd362b05a29a
Vinicius-Maia04 <vinimaia200@gmail.com>
2024-04-03 22:58:07 -0300

    App E Commerce Updated

--- AppECommerce/app_e_commerce/lib/telaAdmin.dart
+++ AppECommerce/app_e_commerce/lib/telaAdmin.dart
@@ -18,7 +18,7 @@ class _telaAdminState extends State<telaAdmin> {
       body: ListView(
         scrollDirection: Axis.vertical,
         children: [
-          AdminComponents('Teste', 'images/logo.png', 5, 499.90)
+          AdminComponents('Cafeteira Makita', 'images/Cafeteira.jpg')
         ],
       ),
     );
@@ -28,74 +28,129 @@ class _telaAdminState extends State<telaAdmin> {
 class AdminComponents extends StatefulWidget {
   final String nome;
   final String img;
-  final int qtde;
-  final double valor;
-  const AdminComponents(this.nome, this.img, this.qtde, this.valor, {super.key});
+  const AdminComponents(this.nome, this.img, {super.key});
 
   @override
   State<AdminComponents> createState() => AdminComponentsState();
 }
 
 class AdminComponentsState extends State<AdminComponents> {
+  TextEditingController _price = TextEditingController();
+  double price = 0;
+  int qtde = 0;
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Container(
-        child: Row(
+        Padding(padding: EdgeInsets.all(5)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Container(
-              decoration: BoxDecoration(
-                border: Border.all(width: 2),
-                borderRadius: BorderRadius.circular(25)
-              ),
-              height: 196,
-              width: 196,
-              child: Image.asset('${widget.img}'),
+            child: Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 2, color: Colors.redAccent),
+                    borderRadius: BorderRadius.circular(25)
+                  ),
+                  height: MediaQuery.of(context).size.height/4,
+                  width: MediaQuery.of(context).size.width/2,
+                  child: Image.asset('${widget.img}'),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height/4,
+                  width: MediaQuery.of(context).size.width/2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                        children: [
+                          Text('${widget.nome}', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('Valor: R\$${price.toStringAsFixed(2).replaceAll('.', ',')}', style: TextStyle(fontSize: 20),),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Text('Quandidade Disponível: ${qtde}', style: TextStyle(fontSize: 15),),
+                        ],
+                      ),
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text('Qtde: ', style: TextStyle(fontSize: 20),),
+                            ElevatedButton(onPressed: (){
+                              setState(() {
+                                qtde += 1;
+                              });
+                            }, child: Icon(Icons.add),
+                            style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),),
+                            ElevatedButton(onPressed: (){
+                              setState(() {
+                                qtde -= 1;
+                                if (qtde<0){
+                                  qtde = 0;
+                                }
+                              });
+                            }, child: Icon(Icons.remove),
+                            style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),)
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text('Preço:', style: TextStyle(fontSize: 20),),
+                          Container(
+                            height: 35,
+                            width: 75,
+                            child: TextField(
+                              controller: _price,
+                              cursorColor: Colors.red,
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(5),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.grey,width: 2),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                  borderSide: BorderSide(color: Colors.red, width: 4)
+                                )
+                                
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(onPressed: () {
+                            if (_price.text.contains(',')){
+                              _price.text = _price.text.toString().replaceAll(',', '.');
+                            };
+                            setState(() {
+                              price = double.tryParse(_price.text) ?? 0;
+                              
+                            });
+                          }, 
+                          child: Icon(Icons.check),
+                          style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red),
+                          ),
+                          ),
+                        ]
+                      )
+                    ],
+                  ),
+                )
+              ],
             ),
-            Container(
-              height: 196,
-              width: 196,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    children: [
-                      Text('${widget.nome}', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Valor: ${widget.valor}', style: TextStyle(fontSize: 20),),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text('Quandidade Disponível: ${widget.qtde}', style: TextStyle(fontSize: 15),),
-                    ],
-                  ),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('Qtde: ', style: TextStyle(fontSize: 20),),
-                        ElevatedButton(onPressed: (){
-                          
-                        }, child: Icon(Icons.plus_one),
-                        style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),),
-                        ElevatedButton(onPressed: (){
-      
-                        }, child: Icon(Icons.replay_outlined),
-                        style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(Colors.red)),)
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
+          ),
           ],
         ),
-      ),
       ],
     );
   }
